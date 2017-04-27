dc3040db6bd9658ebbd0ffde9ed48008db30a6d2
Stanley Phu <sphu@ucsd.edu>
2017-04-26 23:39:29 -0700

    Style about page & link to github profile

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on React. I have built this for
+        The Complete React Web App Developer Course.
+      </p>
+      <h5><u>Here is some information about me</u></h5>
+      <p>
+        I graduated from the University of California, San Diego with a
+        B.S. Electrical Engineering and my depth in Software Systems. I
+        currently work at Greenlee Communications in Vista, California
+        as an Electrical Engineer.
+      </p>
+      <a href='https://github.com/stanleyphu'>Github Profile</a>
     </div>
   );
 };
